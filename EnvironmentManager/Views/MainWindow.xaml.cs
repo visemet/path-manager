91a1de81b6b657dfac75b68022aec58cc95b63dd
Max Hirschhorn <maxh@caltech.edu>
2013-01-08 19:25:50 -0800

    Created MainWindow view.
    Displays a menu with options to import or export an environment
    configuration.
    
    Has two tabs: one for system variables, and the other for user variables.
    Each pane has two data grids: one for displaying the variable name and
    state, and the other for displaying the value state and content.
    
    Unfortunately hidden columns with a default value specified do not
    participate in the creation of a new item, so the OnInitializingNewItem
    handlers were a workaround to set the type of variable correctly.

---
+++ EnvironmentManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Visemet">
+//     Copyright (c) Visemet. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Visemet.Environment
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" />
+        /// class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the new variable as a system variable.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Provides the data for the event.</param>aram>
+        private void OnInitializingSystemVariable(object sender, InitializingNewItemEventArgs e)
+        {
+            ((Variable)e.NewItem).Type = Variable.VariableType.System;
+        }
+
+        /// <summary>
+        /// Sets the new variable as a user variable.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Provides the data for the event.</param>
+        private void OnInitializingUserVariable(object sender, InitializingNewItemEventArgs e)
+        {
+            ((Variable)e.NewItem).Type = Variable.VariableType.User;
+        }
+    }
+}
